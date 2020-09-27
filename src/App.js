eca23ce4295d2afcab41cf862b3f3e9f86108079
bsAlex11 <baies.alexandru90@yahoo.ro>
2020-09-27 16:06:20 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          adasdadad111
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
+  const linkElement = getByText(/adasdadad111/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
